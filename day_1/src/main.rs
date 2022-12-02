09384d7b3ff3df58e23284cd6886f94fb950563c
pucke-uninow <philippucke@googlemail.com>
2022-12-02 10:40:28 +0100

    refactor(day_1): add elve struct & implement order functionalities & some refactorings

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,5 +1,7 @@
 extern crate core;
 
+use std::cmp::Ordering;
+use std::error::Error;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
@@ -7,41 +9,73 @@ use std::path::Path;
 
 const INPUT_FILE_PATH: &str = "src/input.txt";
 
-fn main() {
+struct Elve {
+    items: Vec<i32>,
+}
+
+impl Elve {
+    fn add_item(&mut self, item: i32) -> () {
+        self.items.push(item)
+    }
+    fn get_total_calories(&self) -> i32 {
+        self.items.iter().sum()
+    }
+}
+
+impl Eq for Elve {}
+
+impl PartialEq<Self> for Elve {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_total_calories().eq(&other.get_total_calories())
+    }
+}
+
+impl PartialOrd<Self> for Elve {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.get_total_calories().partial_cmp(&other.get_total_calories())
+    }
+}
+
+impl Ord for Elve {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.get_total_calories().cmp(&other.get_total_calories())
+    }
+}
+
+fn print_elves_leaderboard(elves: &mut Vec<Elve>, mut top_n: usize) -> () {
+    if top_n > elves.len() {
+        top_n = elves.len();
+    }
+
+    for (index, elve) in elves[elves.len() - top_n..].iter().rev().enumerate() {
+        println!("{}. Elve carries {} calories", index + 1, elve.get_total_calories());
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let path = Path::new(INPUT_FILE_PATH);
-    let display = path.display();
+    let file = File::open(&path)?;
 
-    let file = match File::open(&path) {
-        Err(why) => panic!("could not open {}: {}", display, why),
-        Ok(file) => file
-    };
-
-    let mut calories_sum: Vec<i32> = vec![];
-    let mut temp_calories_count = 0;
+    let mut elves: Vec<Elve> = vec![Elve { items: vec![] }];
 
     for line in io::BufReader::new(&file).lines() {
-        let line_content = match line {
-            Err(why) => panic!("Could not read line: {}", why),
-            Ok(string) => string
-        };
+        let line_content = line?;
 
         if line_content.eq("") {
-            calories_sum.push(temp_calories_count);
-            temp_calories_count = 0;
+            elves.push(Elve { items: vec![] });
             continue;
         }
 
-        let calories = match line_content.parse::<i32>() {
-            Ok(calories) => calories,
-            Err(why) => panic!("Could not parse number to i32 int: {}", why)
-        };
+        let calories = line_content.parse::<i32>()?;
 
-        temp_calories_count += calories;
+        let last_elve_index = elves.len() - 1;
+        elves[last_elve_index].add_item(calories);
     }
-    calories_sum.sort();
 
-    println!("1. Elve has {} Calories", calories_sum[calories_sum.len() - 1]);
-    println!("2. Elve has {} Calories", calories_sum[calories_sum.len() - 2]);
-    println!("3. Elve has {} Calories", calories_sum[calories_sum.len() - 3]);
-    println!("Top 3 Elve's have {} calories in total", calories_sum[calories_sum.len() - 1] + calories_sum[calories_sum.len() - 2] + calories_sum[calories_sum.len() - 3])
+    elves.sort();
+    print_elves_leaderboard(&mut elves, 10);
+
+    println!("Top 3 Elve's have {} calories in total", elves[elves.len() - 3..].iter().fold(0, |a, b| { a + b.get_total_calories() }));
+
+    Ok(())
 }
